233c48850fad9835d46d0cdb1efbfc0a6f5c19f1
AlexGorauvt <alexandru.gora01@e-uvt.ro>
2025-01-12 11:16:24 +0200

    Delete tabel one to one.sql

--- tabel one to one.sql
+++
@@ -1,8 +0,0 @@
-create table sofer
-(
-nume_sofer varchar(100) not null unique,
-serie_buletin char(50) not null unique,
-permis boolean default true,
-varsta int not null,
-serie_masina char(17) not null unique
-);
